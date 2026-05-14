85e54fff329902db7c39904362a14c939532e65c
Reid Sroda <rgsroda@wisc.edu>
2026-05-14 16:05:44 -0500

    AIG-10 AIG-11 AIG-12 AIG-13 AIG-14 AIG-15: round logging with hole by hole score entry, putts, fairways, GIR, and penalties

--- app/App.tsx
+++ app/App.tsx
@@ -7,6 +7,9 @@ import SignInScreen from './src/screens/SignInScreen'
 import SignUpScreen from './src/screens/SignUpScreen'
 import HomeScreen from './src/screens/HomeScreen'
 import ProfileSetupScreen from './src/screens/ProfileSetupScreen'
+import ForgotPasswordScreen from './src/screens/ForgotPasswordScreen'
+import AddRoundScreen from './src/screens/AddRoundScreen'
+import EnterScoresScreen from './src/screens/EnterScoresScreen'
 
 const Stack = createNativeStackNavigator()
 
@@ -50,7 +53,11 @@ export default function App() {
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         {session ? (
           hasProfile ? (
-            <Stack.Screen name="Home" component={HomeScreen} />
+            <>
+              <Stack.Screen name="Home" component={HomeScreen} />
+              <Stack.Screen name="AddRound" component={AddRoundScreen} />
+              <Stack.Screen name="EnterScores" component={EnterScoresScreen} />
+            </>
           ) : (
             <Stack.Screen name="ProfileSetup" component={ProfileSetupScreen} />
           )
@@ -58,6 +65,7 @@ export default function App() {
           <>
             <Stack.Screen name="SignIn" component={SignInScreen} />
             <Stack.Screen name="SignUp" component={SignUpScreen} />
+            <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
           </>
         )}
       </Stack.Navigator>
