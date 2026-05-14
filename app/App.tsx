60995623d60f67cbd7c269747375546fc8b29e96
Reid Sroda <rgsroda@wisc.edu>
2026-05-14 14:35:28 -0500

    AIG-5: build sign up screen

--- app/App.tsx
+++ app/App.tsx
@@ -1,25 +1,5 @@
-import { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
-import { supabase } from './src/lib/supabase'
-
+import SignUpScreen from './src/screens/SignUpScreen'
 
 export default function App() {
-  const [connected, setConnected] = useState(false)
-
-  useEffect(() => {
-    supabase.from('users').select('*').then(({ data, error }) => {
-      if (error) {
-        console.log('Supabase error:', error.message)
-      } else {
-        console.log('Supabase connected!')
-        setConnected(true)
-      }
-    })
-  }, [])
-
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{connected ? 'Supabase connected!' : 'Connecting...'}</Text>
-    </View>
-  )
+  return <SignUpScreen />
 }
